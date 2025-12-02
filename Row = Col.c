5e662ceb5647c6628129de0db68b8542ceb57a78
MD. RAZAUL KARIM <mdrazaulkarim698@gmail.com>
2025-12-02 16:04:10 +0600

    Add files via upload

---
+++ Row = Col.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+
+    int n,row,col;
+    printf("Enter Any Number : ");
+    scanf("%d", &n);
+
+    for(row=1; row<=n;row++)
+    {
+
+        for(col=1; col<=n;col++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+
+    }
+
+    return 0;
+}
