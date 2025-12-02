a2270445acd8ea52267e549970f9ba23acb71419
MD. RAZAUL KARIM <mdrazaulkarim698@gmail.com>
2025-12-02 20:25:44 +0600

    Add files via upload

---
+++ X Star Speacial Charecter.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int n,row,col;
+	printf("Enter Any Number : ");
+	scanf("%d", &n);
+
+	for(row=1;row<=n;row++)
+
+	{
+	    for(col=1;col<=n;col++)
+        {
+            if(row==col || row+col==n+1)
+                printf("*");
+            else
+                printf(" ");
+        }
+
+        printf("\n");
+	}
+
+
+
+return 0;
+}
